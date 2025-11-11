a011e10ad0585c1a23be555067373f3e73e3c20a
Aman Fangeria <amanfangeria980@gmail.com>
2025-11-11 15:12:38 +0530

    feat: product service init & rabbit mq container setup

--- product-service/index.js
+++ product-service/index.js
@@ -1,18 +1,34 @@
 const express = require("express");
-const jwt = require("jsonwebtoken");
+const amqp = require("amqplib");
+
 const app = express();
 const PORT = process.env.PORT_ONE || 8080;
 const mongoose = require("mongoose");
 
 mongoose
     .connect(
-        "mongodb://admin:admin%40123@localhost:27017/product-service"
+        "mongodb://admin:admin%40123@localhost:27017/product-service?authSource=admin"
     )
     .then(() => console.log("✅Product Service DB Connected"))
     .catch((err) => console.error("❌ Some error occurred:", err));
 
 app.use(express.json());
 
+let channel, connection;
+
+async function connectToQueue() {
+    try {
+        const amqpServer = "amqp://admin:admin%40123@localhost:5672";
+        connection = await amqp.connect(amqpServer);
+        channel = await connection.createChannel();
+        await channel.assertQueue("PRODUCT");
+        console.log("✅ Connected to RabbitMQ and 'PRODUCT' queue created");
+    } catch (err) {
+        console.error("❌ RabbitMQ connection error:", err);
+    }
+}
+
+connectToQueue();
 
 app.listen(PORT, () => {
     console.log(`PRODUCT_SERVICE is running on ${PORT}`);
