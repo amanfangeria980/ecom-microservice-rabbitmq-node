325df0dd72c764d083783b11b52b966974925221
Aman Fangeria <amanfangeria980@gmail.com>
2025-11-28 16:59:55 +0530

    complete

--- product-service/index.js
+++ product-service/index.js
@@ -5,7 +5,7 @@ const isAuthenticated = require("../isAuthenticated");
 const app = express();
 const PORT = process.env.PORT_ONE || 8080;
 const mongoose = require("mongoose");
-
+var order;
 mongoose
     .connect(
         "mongodb://admin:admin%40123@localhost:27017/product-service?authSource=admin"
@@ -42,7 +42,28 @@ app.post("/product/create", isAuthenticated, async (req, res) => {
     newProduct.save();
     return res.send(newProduct);
 });
-// Buy a product
+// Buy a product (User sends a list of product ID's to buy)
+// Creating an order with those products and a total value of sum of product's price
+app.post("/product/buy", isAuthenticated, async (req, res) => {
+    const { ids } = req.body;
+    const products = await Product.find({ _id: { $in: ids } });
+    channel.sendToQueue(
+        "ORDER",
+        Buffer.from(
+            JSON.stringify({
+                products,
+                userEmail: req.user.email,
+            })
+        )
+    );
+    channel.consume("PRODUCT", (data) => {
+        console.log("Consuming PRODUCT queue");
+        order = JSON.parse(data.content);
+        channel.ack(data);
+    });
+    return res.json(order);
+});
+
 app.listen(PORT, () => {
     console.log(`PRODUCT_SERVICE is running on ${PORT}`);
 });
